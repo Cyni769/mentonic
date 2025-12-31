610c049eddd04be24b47c04d26fe6350d8702192
Alopino <cyrusmalik769@gmail.com>
2026-01-01 02:12:04 +0530

    first commit

---
+++ secs-v2/mentonic/app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Landing from "@/components/sections/landing";
+
+export default function Page() {
+return (
+  <>
+    <Landing />
+  </>
+);
+}
---
+++ secs-v2/mentonic/components/sections/landing.tsx
@@ -0,0 +1,20 @@
+'use client';
+import React from "react";
+import Link from "next/link";
+import { CircleCheckIcon, CircleHelpIcon, CircleIcon } from "lucide-react";
+
+const Landing = () => {
+  return (
+    <>
+    <section className="bg-white py-16 m-auto max-w-7xl">
+      <div className="container mx-auto px-4">
+        <div className="text-center">
+          <h1 className="text-8xl font-bold mb-4 px-4">Mentonic</h1>
+        </div>
+        </div>
+    </section>
+    </>
+  );
+};
+
+export default Landing;
