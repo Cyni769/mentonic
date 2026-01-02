73fe38d125b808c9b977d97dec9302398ee3763b
Alopino <cyrusmalik769@gmail.com>
2026-01-02 20:29:50 +0530

    small tweak

--- secs-v2/mentonic/components/sections/landing.tsx
+++ secs-v2/mentonic/components/sections/landing.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import React from "react";
 import Link from "next/link";
 import { CircleCheckIcon, CircleHelpIcon, CircleIcon } from "lucide-react";
@@ -6,13 +7,22 @@ import { CircleCheckIcon, CircleHelpIcon, CircleIcon } from "lucide-react";
 const Landing = () => {
   return (
     <>
-    <section className="bg-white py-16 m-auto max-w-7xl">
-      <div className="container mx-auto px-4">
-        <div className="text-center">
-          <h1 className="text-8xl font-bold mb-4 px-4">Mentonic</h1>
-        </div>
-        </div>
-    </section>
+    <div className="top-0 m-4 justify-center w-auto bg-background dark:bg-foreground">
+      <nav className="flex items-center h-16 justify-center p-6 border-primary border gap-x-22">
+        <Link href="/About" className="text-xs1 text-mono text-foreground hover:text-primary dark:hover:bg-primary px-3 py-1 transition">
+           About
+        </Link>
+        <Link href="/Explore" className="text-xs1 text-mono text-foreground ">
+           Explore
+        </Link>
+        <Link href="/Partners" className="text-xs1 text-mono text-foreground">
+           Partners
+        </Link>
+        <Link href="/FQA" className="text-xs1 text-mono text-foreground">
+           FQA
+        </Link>
+        </nav>
+    </div>
     </>
   );
 };
